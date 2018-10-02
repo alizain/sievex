1ada91247260cf2e49d5c7d3d3d5d9831e3275fb
Alizain Feerasta <alizain.feerasta@gmail.com>
2018-10-02 12:45:43 -0400

    Initial commit

---
+++ lib/sievex.ex
@@ -0,0 +1,18 @@
+defmodule Sievex do
+  @moduledoc """
+  Documentation for Sievex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sievex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sievex_test.exs
@@ -0,0 +1,8 @@
+defmodule SievexTest do
+  use ExUnit.Case
+  doctest Sievex
+
+  test "greets the world" do
+    assert Sievex.hello() == :world
+  end
+end
